4d05d92243c5050f7e872d7cf15571b1684e4546
KikiLiii <kate95761@gmail.com>
2023-04-02 22:18:09 +0300

    added viewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.android.unscramble.ui.game
 
-class GameViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+
+class GameViewModel : ViewModel() {
+
+    /*private val viewModel = GameViewModel()*/
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+    }
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    private var score = 0
+    private var currentWordCount = 0
+
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
+
 }
